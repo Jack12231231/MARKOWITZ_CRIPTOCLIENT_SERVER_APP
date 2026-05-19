e9f900dcbe528c1cfd1fda9de19186c386c297f2
Jack12231231 <gleb28032007@gmail.com>
2026-05-19 20:31:59 +0300

    Delete OptimizerFactory.java

--- OptimizerFactory.java
+++
@@ -1,8 +0,0 @@
-package com.example.criptoserver;
-
-public class OptimizerFactory {
-
-    public static PortfolioOptimizer getOptimizer() {
-        return new MarkowitzOptimizer();   // возвращаем реальную реализацию
-    }
-}
